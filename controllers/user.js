9e72141add21ab4363c6206a002f012711e8989d
Hugo <hugomika68@gmail.com>
2021-09-14 16:32:20 +0200

    Finish MVC for User. Next up Food!

--- controllers/user.js
+++ controllers/user.js
@@ -1,31 +1,88 @@
-// Import User model
+// Import models
 const User = require("../models/user");
+const { Food } = require("../models/food");
 
-//TODO create user
-exports.createUser = (req,res) => {
+// Create and save new user
+exports.createUser = async (req, res) => {
+  const userToAdd = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+  });
 
-}
-//TODO findAll user
-exports.findAllUsers = (req,res) => {
-    User.find().then(users => {
-        res.send(users)
+  // Add to DB
+  userToAdd
+    .save()
+    .then(
+      // Return all users in DB
+      res.json(await User.find())
+    )
+    .catch((err) => {
+      res.send(err.message);
     });
-}
-//TODO findOne user
-exports.findOneUser = (req,res) => {
+};
 
-}
-//TODO update user
-exports.updateUser = (req,res) => {
+// Find all users
+exports.findAllUsers = async (req, res) => {
+  res.json(await User.find());
+};
 
-}
-//TODO delete user
-exports.deleteUser = (req,res) => {
+// Find user
+exports.findOneUser = async (req, res) => {
+  const username = req.params.username;
+  res.json(await User.findOne({ username: username }));
+};
 
-}
-//TODO delete all users
-exports.deleteAllUsers = (req,res) => {
+/* Update user
+ Params possible to update: username, food
+*/
+exports.updateUser = async (req, res) => {
+  let username = req.params.username;
 
-}
+  // Look for user in DB
+  const user = await User.findOne({ username: username });
+  if (user === null) {
+    res.json({ message: "User not found." });
+    return;
+  }
 
+  // Look for food in DB and append to user
+  if (req.body.food != null) {
+    const foodToAdd = await Food.findOne({ name: req.body.food });
+    //Spread so it appends instead of replacing the current foods declared
+    if (foodToAdd != null) {
+      user.foods = [...user.foods, foodToAdd];
+      await user.save();
+    }
+  }
 
+  // Update username is key passed
+  if (req.body.username != null) {
+    await User.findOneAndUpdate(
+      { username: username },
+      { username: req.body.username },
+      { new: true }
+    );
+    // Kinda cheating so query below works, if username has been changed look for new name, else look for name defined in url
+    // There has to be a better way but first that came to mind and it works...
+    username = req.body.username;
+  }
+  res.json(await User.findOne({ username: username }));
+};
+
+// Delete user
+exports.deleteUser = async (req, res) => {
+  const username = req.params.username;
+  const result = await User.findOneAndDelete({ username: username });
+  if (result === null) {
+    res.send("User not found");
+  } else {
+    res.send(`User ${username} deleted.`);
+  }
+};
+
+// Delete all users
+exports.deleteAllUsers = async (req, res) => {
+  const result = await User.deleteMany();
+  res.send(`${result.deletedCount} users deleted.`);
+};
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,94 +1,28 @@
 module.exports = (app) => {
+  const user = require("../controllers/user");
   const router = require("express").Router();
 
   // Import models
   const User = require("../models/user");
   const { Food } = require("../models/food");
 
-  // TODO implement REST API for users
-  //COMPLETED findAll user
-  router.get("/", async (req, res) => {
-    res.json(await User.find());
-  });
+  // Find all users
+  router.get("/", user.findAllUsers);
 
-  //COMPLETED create user
-  router.post("/", (req, res) => {
-    const userToAdd = new User({
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email,
-    });
-    console.log(req.body.username);
+  // Create user
+  router.post("/", user.createUser);
 
-    // Add to DB
-    userToAdd
-      .save()
-      .then(async () => {
-        // Return all users in DB
-        res.json(await User.find());
-      })
-      .catch((err) => {
-        res.send(err.message);
-      });
-  });
+  // Find one user
+  router.get("/:username", user.findOneUser);
 
-  //Completed findOne user
-  router.get("/:username", async (req, res) => {
-    const username = req.params.username;
-    res.json(await User.findOne({ username: username }));
-  });
+  // Update user
+  router.post("/:username", user.updateUser);
 
-  //Completed update user
-  router.post("/:username", async (req, res) => {
-    let username = req.params.username;
+  // Delete user
+  router.delete("/:username", user.deleteUser);
 
-    // Look for user in DB
-    const user = await User.findOne({ username: username });
-    if (user === null) {
-      res.json({ message: "User not found." });
-      return;
-    }
-
-    // Look for food in DB and append to user
-    if (req.body.food != null) {
-      const foodToAdd = await Food.findOne({ name: req.body.food });
-      //Spread so it appends instead of replacing the current foods declared
-      if (foodToAdd != null) {
-          user.foods = [...user.foods, foodToAdd];
-          await user.save();
-      }
-    }
-
-    // Update username is key passed
-    if (req.body.username != null) {
-      await User.findOneAndUpdate(
-        { username: username },
-        { username: req.body.username },
-        { new: true }
-      );
-      // Kinda cheating so query below works, if username has been changed look for new name, else look for name defined in url
-      // There has to be a better way but first that came to mind and it works...
-      username = req.body.username;
-    }
-    res.json(await User.findOne({ username: username }));
-  });
-
-  //Completed delete user
-  router.delete("/:username", async (req, res) => {
-    const username = req.params.username;
-    const result = await User.findOneAndDelete({ username: username });
-    if (result === null) {
-      res.send("User not found");
-    } else {
-      res.send(`User ${username} deleted.`);
-    }
-  });
-
-  //Completed delete all users
-  router.delete("/", async (req, res) => {
-    const result = await User.deleteMany();
-    res.send(`${result.deletedCount} users deleted.`);
-  });
+  // Delete all users
+  router.delete("/", user.deleteAllUsers);
 
   app.use("/users", router);
 };
