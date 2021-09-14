015926e59799480e3d972bf37ffe061879e53708
Hugo <hugomika68@gmail.com>
2021-09-14 16:34:23 +0200

    Cleanup routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,15 +2,14 @@ module.exports = (app) => {
   const user = require("../controllers/user");
   const router = require("express").Router();
 
-  // Import models
-  const User = require("../models/user");
-  const { Food } = require("../models/food");
-
   // Find all users
   router.get("/", user.findAllUsers);
 
   // Create user
   router.post("/", user.createUser);
+  
+  // Delete all users
+  router.delete("/", user.deleteAllUsers);
 
   // Find one user
   router.get("/:username", user.findOneUser);
@@ -20,9 +19,6 @@ module.exports = (app) => {
 
   // Delete user
   router.delete("/:username", user.deleteUser);
-
-  // Delete all users
-  router.delete("/", user.deleteAllUsers);
 
   app.use("/users", router);
 };
