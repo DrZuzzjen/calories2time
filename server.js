0df44bd94a5144c3b799d1ba3c85539d71b2975a
Hugo <hugomika68@gmail.com>
2021-09-16 17:26:44 +0200

    Fix food rest api

--- server.js
+++ server.js
@@ -50,5 +50,59 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-
+// Import user routes
 require("./routes/user_routes")(app);
+
+// Food REST API
+// Get all foods
+app.get("/foods", async (req, res) => {
+  res.json(await Food.find());
+});
+
+// Add a food
+app.post("/foods", async (req, res) => {
+  // Create food object from body request
+  const foodToAdd = new Food({
+    name: req.body.name,
+    time: req.body.time,
+  });
+
+  // Save in DB
+  foodToAdd
+    .save()
+    .then(res.json(await Food.find()))
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+// Delete all food
+app.delete("/foods", async (req, res) => {
+  const {deletedCount} = await Food.deleteMany();
+  res.send(`Deleted ${deletedCount} foods`);
+});
+
+// Get a food
+app.get("/foods/:food", async (req, res) => {
+  res.json(await Food.findOne({ name: req.params.food }));
+});
+
+// Update a food
+app.post("/foods/:food", async (req, res) => {
+  const newTime = req.body.time;
+  res.json(
+    await Food.findOneAndUpdate({ name: req.params.food }, { time: newTime }, {new: true})
+  );
+});
+
+// Delete a food
+app.delete("/foods/:food", async (req, res) => {
+  const food = req.params.food;
+  const result = await Food.findOneAndDelete({ name: food });
+  console.log(result);
+  if (result === null) {
+    res.send("Food not found");
+  } else {
+    res.send(`Food ${food} deleted.`);
+  }
+});
